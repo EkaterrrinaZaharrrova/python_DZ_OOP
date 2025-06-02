15a1a17a879c235bb8f4c330edf8a9bf24bc22a4
Екатерина Захарова <k6orodaenko@yandex.ru>
2025-06-02 23:17:51 +0300

    add new func

--- src/class_category.py
+++ src/class_category.py
@@ -6,13 +6,27 @@ class Category:
     description: str
     category_count: int = 0
     product_count: int = 0
-    products: list[Product]
+    __products: list[Product]
 
     def __init__(self, name: str, description: str, products: list[Product]):
         """Инициализация класса категории"""
 
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         self.category_count += 1
         self.product_count += len(products) if products else 0
+
+
+    def add_product(self, product: Product):
+        if isinstance(self.products, Product):
+            self.__products.append(product)
+            self.product_count += 1
+
+
+    @property
+    def products(self):
+        product_info = ""
+        for elem in self.__products:
+            product_info += f"{elem.name}, {elem.price} руб. Остаток: {elem.quantity} шт.\n"
+        return product_info
---
+++ src/class_product.py
@@ -0,0 +1,35 @@
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name: str, description: str, price: float, quantity: int):
+        """Инициализация класса продукт."""
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+
+
+    @classmethod
+    def new_product(cls, new_product: dict):
+        """Метод создания нового объекта Product"""
+        new_pro = cls(**new_product)
+        return new_pro
+
+
+    @property
+    def price(self):
+        """Геттер"""
+
+        return self.__price
+
+    @price.setter
+    def price(self, set_price: float) -> None:
+        """Сеттер, значения цены"""
+
+        if set_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = set_price
