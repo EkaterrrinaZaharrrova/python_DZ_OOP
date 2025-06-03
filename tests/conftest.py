3135f0bce3675736bc7949674109789050c55db6
Екатерина Захарова <k6orodaenko@yandex.ru>
2025-06-03 14:43:25 +0300

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,3 +15,22 @@ def category_fixture():
     """Фикстура для класса категории."""
     return Category("Смартфоны",
                          "Смартфоны - средство получения дополнительных функций для удобства жизни", [])
+
+
+@pytest.fixture
+def count_category_fixture() -> list:
+
+    return [
+        {"name": "Zeekr 001 FR", "description": "256GB", "price": 6_250_000, "quantity": 1},
+        {"name": "Voyah Free", "description": "256GB", "price": 4_360_000, "quantity": 3},
+        {"name": "Geely Monjaro", "description": "256GB", "price": 3_230_000, "quantity": 14},
+        {"name": "Staria", "description": "256GB", "price": 5_250_000, "quantity": 10},
+        {"name": "Lixiang L9", "description": "256GB", "price": 5_360_000, "quantity": 3},
+        {"name": "TANK 700", "description": "256GB", "price": 7_230_000, "quantity": 4},
+        {"name": "BMW X7", "description": "256GB", "price": 16_450_777, "quantity": 2},
+        {"name": "MB GLS MAY", "description": "256GB", "price": 46_777_777, "quantity": 1},
+        {"name": "MB VIANO", "description": "256GB", "price": 6_450_777, "quantity": 6},
+        {"name": "Maybach 62", "description": "256GB", "price": 86_777_777, "quantity": 1},
+        {"name": "Audi TT", "description": "256GB", "price": 7_450_777, "quantity": 2},
+        {"name": "MB GLS ", "description": "256GB", "price": 26_757_757, "quantity": 3}
+    ]
